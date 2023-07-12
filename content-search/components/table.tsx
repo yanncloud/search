d74c98f8293c12a5539328021e79b180a35c0595
Yannick Stritzke <yannick-stritzke@outlook.de>
2023-07-12 13:15:21 +0200

    deleted checked
    deleted checked

--- content-search/components/table.tsx
+++ content-search/components/table.tsx
@@ -106,7 +106,6 @@ function JsonDataDisplayTeam(): ReactElement {
                       id="vue-checkbox-list"
                       type="checkbox"
                       value=""
-                      checked=""
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                     />
                     <label
